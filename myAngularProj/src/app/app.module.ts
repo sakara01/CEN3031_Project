c2a9f8c82d05fd4a72e36e99cf7b837ef123370f
Justus <73701569+justusgeslani@users.noreply.github.com>
2023-02-21 20:00:11 -0500

    caffinder banner/header

--- myAngularProj/src/app/app.module.ts
+++ myAngularProj/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { GoogleMapsModule } from '@angular/google-maps';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
