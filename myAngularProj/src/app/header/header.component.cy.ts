01062514523328191411fae67b6ee74c17a8cc65
Lauren Escarcha <laurenescarcha@ufl.edu>
2023-03-01 10:24:52 -0500

    cypress test - search button clicked

--- myAngularProj/src/app/header/header.component.cy.ts
+++ myAngularProj/src/app/header/header.component.cy.ts
@@ -6,4 +6,10 @@ describe('HeaderComponent', () => {
         cy.mount(HeaderComponent)
     })
 
+    //tests only the search button's click
+    it('button can be clicked', () => {
+        cy.mount(HeaderComponent)
+        cy.get('#header > #searchBtn').click()
+      })
+
 })
