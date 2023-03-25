a62d4bdbfc25bd936f010b47528e0dba5edf2c56
Justus <73701569+justusgeslani@users.noreply.github.com>
2023-03-24 21:30:12 -0400

    angular unit tests for login component

--- myAngularProj/src/app/login/login.component.spec.ts
+++ myAngularProj/src/app/login/login.component.spec.ts
@@ -20,4 +20,37 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it('should display Login title', () => {
+    const h2: HTMLElement = fixture.nativeElement.querySelector('h2');
+    expect(h2.innerHTML).toBe("Login");
+  });
+
+  it('should display option to enter Username and Password',()=>{
+    expect(component.publicUsername).toBe("empty");
+    expect(component.userData).toBe(undefined);
+  });
+
+  it('should check click options to Create Account, Log Out, Close Window', fakeAsync(()=>{
+    spyOn(component,'createAcct');
+    let createAcct = fixture.debugElement.nativeElement.querySelector('#createAcct');
+    createAcct.click();
+    tick();
+    expect(component.createAcct).toHaveBeenCalled();
+
+    spyOn(component,'clearModal');
+    let logout = fixture.debugElement.nativeElement.querySelector('#logout');
+    logout.click();
+    tick();
+    expect(component.clearModal).toHaveBeenCalled();
+
+    spyOn(component,'closeModal');
+    let closeBtn = fixture.debugElement.nativeElement.querySelector('#closeBtn');
+    closeBtn.click();
+    tick();
+    expect(component.closeModal).toHaveBeenCalled();
+    
+  }));
+
+  
 });
