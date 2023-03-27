266c1a9a1a258ae8ac3219af348f90c7ce2d1051
Saloni Karanth <salonikaranth01@gmail.com>
2023-03-26 21:51:49 -0400

    fixed broken cypress tests

--- myAngularProj/src/app/header/header.component.cy.ts
+++ myAngularProj/src/app/header/header.component.cy.ts
@@ -1,8 +1,23 @@
 import { HeaderComponent } from "./header.component"
 import { RouterTestingModule } from "@angular/router/testing"
 import { Component } from "@angular/core"
+import { LoginComponent } from '../login/login.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { FavoritesComponent } from 'app/favorites/favorites.component';
+import { TestBed, async } from '@angular/core/testing';
 
 describe('HeaderComponent', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+          imports: [
+            HttpClientTestingModule,
+          ],
+          declarations: [
+            HeaderComponent, LoginComponent, FavoritesComponent
+          ],
+          providers:[LoginComponent,FavoritesComponent]
+        }).compileComponents();
+      }));
 
     it('can mount', () => {
         cy.mount(HeaderComponent)
