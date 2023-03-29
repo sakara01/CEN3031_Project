f6c8bc28fa49f1721861a6af5661f10179c8b1cb
Lauren Escarcha <laurenescarcha@ufl.edu>
2023-03-29 01:12:03 -0400

    wow visit localhost e2e test :D

--- myAngularProj/cypress/e2e/spec.cy.ts
+++ myAngularProj/cypress/e2e/spec.cy.ts
@@ -1,11 +1,5 @@
 describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
-  })
-})
-
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(false)
+  it('Visits the Kitchen Sink', () => {
+    cy.visit('http://localhost:4200/')
   })
 })
