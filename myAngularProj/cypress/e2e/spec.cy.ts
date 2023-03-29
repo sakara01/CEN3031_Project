ddcca8075387ee951251d196d177f8b3300c43dc
karentong14 <karentong@ufl.edu>
2023-03-29 02:09:55 -0400

    Cypress test for login

--- myAngularProj/cypress/e2e/spec.cy.ts
+++ myAngularProj/cypress/e2e/spec.cy.ts
@@ -1,5 +1,19 @@
-describe('My First Test', () => {
-  it('Visits the Kitchen Sink', () => {
+describe('Test Login', () => {
+  it('Logs in to Account', () => {
     cy.visit('http://localhost:4200/')
+
+    cy.get('#searchBtn').click()
+
+    cy.get('#loginButton').click()
+
+    cy.get('#nameGiven').click()
+    cy.get('#nameGiven').type('User123')
+
+    cy.get('#passGiven').click()
+    cy.get('#passGiven').type('password123')
+
+    cy.get('#submitBtn').click()
+    cy.get('#createAcct').click()
+    cy.get('#closeBtn').click()
   })
 })
