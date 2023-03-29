2e61b31c34241d8b1d2c13c2ab5b13aa8c5b6d54
Lauren Escarcha <laurenescarcha@ufl.edu>
2023-03-29 05:37:05 -0400

    more e2e tests

--- myAngularProj/cypress/e2e/loginActions.cy.ts
+++ myAngularProj/cypress/e2e/loginActions.cy.ts
@@ -18,6 +18,36 @@ describe('Test Login & search button', () => {
   })
 })
 
+describe('Test Incorrect Login, login & logout', () => {
+  it('Logs in to Account', () => {
+    cy.visit('http://localhost:4200/')
+
+    cy.get('#searchBtn').click()
+
+    cy.get('#loginButton').click()
+
+    //incorrect user n pass
+    cy.get('#nameGiven').click()
+    cy.get('#nameGiven').type('urmom')
+
+    cy.get('#passGiven').click()
+    cy.get('#passGiven').type('urdad')
+
+    //correct user n pass
+    cy.get('#nameGiven').click()
+    cy.get('#nameGiven').type('urmom')
+
+    cy.get('#passGiven').click()
+    cy.get('#passGiven').type('urmom')
+
+    cy.get('#loginButton').click()
+
+    //logout after logged in
+    cy.get('#logout').click()
+
+  })
+})
+
 describe('Test Saved Favs', () => {
   it('Checks Favs After Login', () => {
     cy.visit('http://localhost:4200/')
@@ -37,3 +67,21 @@ describe('Test Saved Favs', () => {
     cy.get('#showFavs').click()
   })
 })
+
+//realized cant test side bar without clicking on the map pins
+// describe('Test Add Favs', () => {
+//   it('Adds Favs After Login', () => {
+//     cy.visit('http://localhost:4200/')
+
+//     cy.get('#loginButton').click()
+
+//     cy.get('#nameGiven').click()
+//     cy.get('#nameGiven').type('urmom')
+
+//     cy.get('#passGiven').type('urmom')
+    
+//     cy.get('#submitBtnHolder').click()
+
+//     cy.get('nearbyPlaces').invoke(openSId)
+//   })
+// })
